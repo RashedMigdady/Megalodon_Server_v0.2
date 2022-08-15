6123d4fe1e8fb19bf03216ef6370d0aa4bf98e23
Rashed <migdadyrashed@gmail.com>
2022-08-15 14:35:29 +0300

    ADD KEYs VALUES TO .ENV

--- db/db.js
+++ db/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 require('dotenv').config();
-
+ 
 const connection = mysql.createConnection({
-  host: "sql6.freesqldatabase.com",
-  user: "sql6512952",
-  password: "Liqt7nedXm",
-  database: "sql6512952",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB,
 });
 
 connection.connect((err) => {
