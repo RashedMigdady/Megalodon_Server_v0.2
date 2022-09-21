342948fa321cb10996a35b0e9b3494411d65cd1b
Rashed <migdadyrashed@gmail.com>
2022-09-21 13:18:52 +0300

    update lookup id countries

--- routers/controllers/LookUpsIds/LookUpsIds.js
+++ routers/controllers/LookUpsIds/LookUpsIds.js
@@ -1,7 +1,7 @@
 const connection = require('../../../db/db');
  
 const LookUpsIdCountries = (req, res) => {
-    const { lookUpId } = req.body;
+    const { lookUpId } = req.params;
 
     const query = `select * from countries`;
 
@@ -13,7 +13,7 @@ const LookUpsIdCountries = (req, res) => {
                 err: err,
             });
         }
-        if (lookUpId == 231)
+        if (lookUpId == '231')
         return res.status(200).json({
             success: true,
             message: `get all Countries is done`,
--- routers/routes/LookUpsIds/LookUpsIds.js
+++ routers/routes/LookUpsIds/LookUpsIds.js
@@ -3,7 +3,6 @@ const { LookUpsIdCountries } = require('../../controllers/LookUpsIds/LookUpsIds'
 const authentication = require("../../../routers/middlewares/authentication");
 
 const LookUpsIdsRouter = express.Router();
-LookUpsIdsRouter.get('/Countries', authentication, LookUpsIdCountries);
+LookUpsIdsRouter.get('/Countries/:lookUpId',authentication, LookUpsIdCountries);
 
 module.exports = LookUpsIdsRouter;
-
