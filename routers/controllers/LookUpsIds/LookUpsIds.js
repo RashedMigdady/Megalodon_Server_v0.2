881044ce230fef24b14f7fbbc393e8c435f82c0a
Rashed <migdadyrashed@gmail.com>
2022-10-09 10:44:49 +0300

    create LookUpId for Sports

--- routers/controllers/LookUpsIds/LookUpsIds.js
+++ routers/controllers/LookUpsIds/LookUpsIds.js
@@ -1,5 +1,5 @@
 const connection = require('../../../db/db');
- 
+
 const LookUpsIdCountries = (req, res) => {
     const { lookUpId } = req.params;
 
@@ -14,16 +14,44 @@ const LookUpsIdCountries = (req, res) => {
             });
         }
         if (lookUpId == '231')
-        return res.status(200).json({
-            success: true,
-            message: `get all Countries is done`,
-            countries: result
-        });
+            return res.status(200).json({
+                success: true,
+                message: `get all Countries is done`,
+                countries: result
+            });
         else
-        return res.status(500).json({
-            success: false,
-            message: `There is wrong in lookup id code.`,
-        });
+            return res.status(500).json({
+                success: false,
+                message: `There is wrong in lookup id code.`,
+            });
     })
 }
-module.exports = {LookUpsIdCountries}
+
+
+const LookUpsIdSports = (req, res) => {
+    const { lookUpId } = req.params;
+
+    const query = `select * from sports where is_delete = 0`;
+    connection.query(query, (err, result) => {
+        if (err) {
+            return res.status(500).json({
+                success: false,
+                message: `Server Error`,
+                err: err,
+            })
+        }
+
+        if (lookUpId == '99')
+            return res.status(200).json({
+                success: true,
+                message: `get all Sports is done`,
+                sports: result
+            })
+        else
+            return res.status(500).json({
+                success: false,
+                message: `There is wrong in lookup id code.`,
+            });
+    })
+}
+module.exports = { LookUpsIdCountries, LookUpsIdSports }
--- routers/routes/LookUpsIds/LookUpsIds.js
+++ routers/routes/LookUpsIds/LookUpsIds.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { LookUpsIdCountries } = require('../../controllers/LookUpsIds/LookUpsIds');
+const { LookUpsIdCountries, LookUpsIdSports } = require('../../controllers/LookUpsIds/LookUpsIds');
 const authentication = require("../../../routers/middlewares/authentication");
 
 const LookUpsIdsRouter = express.Router();
 LookUpsIdsRouter.get('/Countries/:lookUpId',authentication, LookUpsIdCountries);
+LookUpsIdsRouter.get('/Sports/:lookUpId', authentication, LookUpsIdSports);
 
 module.exports = LookUpsIdsRouter;
